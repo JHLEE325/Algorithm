a9bba71df9de171b0d5ff118310b6e4d05edf9de
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-02-25 09:18:19 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int X = Integer.parseInt(br.readLine());
+		int count = 0;
+		for (int i = 0; i <= 6; i++) {
+			if ((X & 1 << i) != 0)
+				count++;
+		}
+
+		System.out.println(count);
+	}
+
+}
