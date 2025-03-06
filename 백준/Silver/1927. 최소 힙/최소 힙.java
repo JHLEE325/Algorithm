a2dfc90fa3387d266bb79098e8cfbf2f4d025894
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-03-06 09:27:20 +0900

    [Silver II] Title: 최소 힙, Time: 248 ms, Memory: 27392 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		// --------------솔루션 코드를 작성하세요.--------------------------------
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
