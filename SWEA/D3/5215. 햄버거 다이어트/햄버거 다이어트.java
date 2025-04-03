b979d2efb69414e6f2dc4d1e75c859895084d418
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-04-03 14:36:43 +0900

    [D3] Title: 햄버거 다이어트, Time: 112 ms, Memory: 35,904 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,65 +3,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static int[] numbers;
-	static int M,N,L,mscore=0;
-	static int[] score;
-	static int[] cal;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(st.nextToken());
-		
-		for(int t=1;t<=T;t++) {
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			L = Integer.parseInt(st.nextToken());
-			mscore=0;
-			
-			score = new int[N];
-			cal = new int[N];
-			
-			for(int i=0;i<N;i++) {
+			int n = Integer.parseInt(st.nextToken());
+			int maxcal = Integer.parseInt(st.nextToken());
+			int[] flavors = new int[n];
+			int[] calories = new int[n];
+
+			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				score[i] = Integer.parseInt(st.nextToken());
-				cal[i] = Integer.parseInt(st.nextToken());
+				flavors[i] = Integer.parseInt(st.nextToken());
+				calories[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			for(int i=1; i<=N; i++) {
-				M=i;
-				numbers = new int[M];
-				dice4(0,0);
-			}
-			sb.append("#"+t+" "+mscore+"\n");
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static void dice4(int cnt, int start) { // 조합
-		if (cnt == M) {
-			int tmpscore=0;
-			int tmpcal=0;
-			for(int i=0;i<M;i++) {
-				tmpcal+=cal[numbers[i]];
-				tmpscore+=score[numbers[i]];
-			}
-			if(tmpcal<=L) {
-				if(mscore<tmpscore) {
-					mscore=tmpscore;
+
+			int[][] dp = new int[n + 1][maxcal + 1];
+
+			for (int i = 1; i <= n; i++) {
+				for (int c = 0; c <= maxcal; c++) {
+					if (calories[i - 1] > c) {
+						dp[i][c] = dp[i - 1][c];
+					} else {
+						dp[i][c] = Math.max(dp[i - 1][c], dp[i - 1][c - calories[i - 1]] + flavors[i - 1]);
+					}
 				}
 			}
-			return;
-		}
 
-		for (int i = start; i < N; i++) {
-			numbers[cnt] = i;
-			dice4(cnt + 1, i + 1);
+			sb.append("#" + t + " " + dp[n][maxcal] + "\n");
 		}
+		System.out.println(sb);
 	}
-
 }
