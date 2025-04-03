3894dc0994413d8b93895b15d46f0edcd392ef4a
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-04-03 14:37:43 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 86 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -4,9 +4,6 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-	static int[] swim, pricelist;
-	static int result;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -15,41 +12,31 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 1; t <= T; t++) {
-			pricelist = new int[4];
-			swim = new int[15];
-			result = Integer.MAX_VALUE;
-
 			st = new StringTokenizer(br.readLine());
+			int[] price = new int[4];
 			for (int i = 0; i < 4; i++) {
-				pricelist[i] = Integer.parseInt(st.nextToken());
+				price[i] = Integer.parseInt(st.nextToken());
 			}
 
 			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= 12; i++) {
-				swim[i] = Integer.parseInt(st.nextToken());
+			int[] month = new int[13];
+			for (int i = 1; i < 13; i++) {
+				month[i] = Integer.parseInt(st.nextToken());
 			}
-			calprice(1, 0);
-			sb.append("#" + t + " " + result + "\n");
+
+			int[] dp = new int[13];
+			for (int i = 1; i < 13; i++) {
+				int cost1 = Math.min(dp[i - 1] + price[0] * month[i], dp[i - 1] + price[1]);
+				int cost2 = price[2];
+				if (i >= 3) {
+					cost2 = dp[i - 3] + price[2];
+				}
+
+				dp[i] = Math.min(cost1, cost2);
+			}
+
+			sb.append("#" + t + " " + Math.min(dp[12], price[3]) + "\n");
 		}
 		System.out.println(sb);
 	}
-
-	public static void calprice(int month, int price) {
-		if (month > 12) {
-			if (result > price) {
-				result = price;
-			}
-			return;
-		}
-		if (swim[month] == 0) {
-			calprice(month + 1, price);
-		} else {
-			calprice(month + 1, price + (swim[month] * pricelist[0]));
-			calprice(month + 1, price + (pricelist[1]));
-			calprice(month + 3, price + (pricelist[2]));
-			calprice(month + 12, price + (pricelist[3]));
-		}
-
-	}
-
 }
