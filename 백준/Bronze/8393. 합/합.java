796ff9252a44f3a2837413b459e9fc2ae00f4ca6
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-08-19 17:27:28 +0900

    [Bronze V] Title: 합, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        
+        System.out.println(sum);
+    }
+}
