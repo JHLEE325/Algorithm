4a24c3bf0dd85cb4f7902e9751fca810c2a62d95
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-08-29 15:12:36 +0900

    [Gold III] Title: 파티, Time: 128 ms, Memory: 16736 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,126 +1,111 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-class Node implements Comparable<Node> {
-	int index;
-	int weight;
-
-	public Node(int index, int cost) {
-		this.index = index;
-		this.weight = cost;
-	}
-
-	@Override
-	public int compareTo(Node o) {
-		return Integer.compare(this.weight, o.weight);
-	}
-}
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int[] arr = new int[101]; // 사다리와 뱀의 정보를 저장하는 배열
-	static boolean[] visited = new boolean[101]; // 방문 여부를 체크하는 배열
-	static int n, m, x;
-	static ArrayList<Node>[] graph;
-	static ArrayList<Node>[] graph2;
+    static class Node implements Comparable<Node> {
+        int to, cost;
 
-	public static void main(String[] args) throws Exception {
+        public Node(int t, int c) {
+            this.to = t;
+            this.cost = c;
+        }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		x = Integer.parseInt(st.nextToken());
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
 
-		graph = new ArrayList[n + 1];
-		graph2 = new ArrayList[n + 1];
+    static int n, m, x;
+    static ArrayList<Node>[] graph1;
+    static ArrayList<Node>[] graph2;
 
-		for (int i = 0; i <= n; i++) {
-			graph[i] = new ArrayList<>();
-			graph2[i] = new ArrayList<>();
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
 
-			graph[v].add(new Node(u, w));
-			graph2[u].add(new Node(v, w));
-		}
+        graph1 = new ArrayList[n + 1];
+        graph2 = new ArrayList[n + 1];
 
-		int[] dist = Dijkstra(x);
-		int[] dist2 = Dijkstra2(x);
-		int max = 0;
-		for (int i = 1; i <= n; i++) {
-			max = Math.max(max, dist[i] + dist2[i]);
-		}
-		System.out.println(max);
-	}
+        for (int i = 0; i <= n; i++) {
+            graph1[i] = new ArrayList<>();
+            graph2[i] = new ArrayList<>();
+        }
 
-	public static int[] Dijkstra(int start) {
-		boolean[] check = new boolean[n + 1];
-		int[] dist = new int[n + 1];
-		int INF = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph1[from].add(new Node(to, cost));
+            graph2[to].add(new Node(from, cost));
+        }
+        int[] dist = Dijkstra(x);
+        int[] dist2 = Dijkstra2(x);
+        int max = -1;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, dist[i] + dist2[i]);
+        }
+        System.out.println(max);
+    }
 
-		Arrays.fill(dist, INF);
-		dist[start] = 0;
+    public static int[] Dijkstra(int start) {
+        boolean[] check = new boolean[n + 1];
+        int[] dist = new int[n + 1];
 
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
+        Arrays.fill(dist, 987654321);
+        dist[start] = 0;
 
-		while (!pq.isEmpty()) {
-			int nowVertex = pq.poll().index;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
 
-			if (check[nowVertex])
-				continue;
-			check[nowVertex] = true;
+        while (!pq.isEmpty()) {
+            int cur = pq.poll().to;
 
-			for (Node next : graph[nowVertex]) {
-				if (dist[next.index] > dist[nowVertex] + next.weight) {
-					dist[next.index] = dist[nowVertex] + next.weight;
+            if (check[cur])
+                continue;
+            check[cur] = true;
 
-					pq.offer(new Node(next.index, dist[next.index]));
-				}
-			}
-		}
+            for (Node next : graph1[cur]) {
+                if (dist[next.to] > dist[cur] + next.cost) {
+                    dist[next.to] = dist[cur] + next.cost;
+                    pq.offer(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
 
-		return dist;
-	}
+        return dist;
+    }
 
-	public static int[] Dijkstra2(int start) {
-		boolean[] check = new boolean[n + 1];
-		int[] dist = new int[n + 1];
-		int INF = Integer.MAX_VALUE;
+    public static int[] Dijkstra2(int start) {
+        boolean[] check = new boolean[n + 1];
+        int[] dist = new int[n + 1];
 
-		Arrays.fill(dist, INF);
-		dist[start] = 0;
+        Arrays.fill(dist, 987654321);
+        dist[start] = 0;
 
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(start, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
 
-		while (!pq.isEmpty()) {
-			int nowVertex = pq.poll().index;
+        while (!pq.isEmpty()) {
+            int cur = pq.poll().to;
 
-			if (check[nowVertex])
-				continue;
-			check[nowVertex] = true;
+            if (check[cur])
+                continue;
+            check[cur] = true;
 
-			for (Node next : graph2[nowVertex]) {
-				if (dist[next.index] > dist[nowVertex] + next.weight) {
-					dist[next.index] = dist[nowVertex] + next.weight;
-
-					pq.offer(new Node(next.index, dist[next.index]));
-				}
-			}
-		}
-		return dist;
-	}
+            for (Node next : graph2[cur]) {
+                if (dist[next.to] > dist[cur] + next.cost) {
+                    dist[next.to] = dist[cur] + next.cost;
+                    pq.offer(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+        return dist;
+    }
 }
