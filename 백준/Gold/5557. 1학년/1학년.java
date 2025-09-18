b20513c1ff14bfb4f2d38e6fbd51c0758de0654b
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-09-19 07:58:17 +0900

    [Gold V] Title: 1학년, Time: 68 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        long[][] dp = new long[n - 1][21];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0][arr[0]] = 1;
+
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 0; j <= 20; j++) {
+                int plus = j + arr[i];
+                int minus = j - arr[i];
+
+                if (plus <= 20)
+                    dp[i][plus] += dp[i - 1][j];
+                if (minus >= 0)
+                    dp[i][minus] += dp[i - 1][j];
+            }
+        }
+
+        System.out.println(dp[n - 2][arr[n - 1]]);
+    }
+}
