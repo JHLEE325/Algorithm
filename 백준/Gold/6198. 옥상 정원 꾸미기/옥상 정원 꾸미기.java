c5e1d01aeeafdeaab318113efa855576e79001a9
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-11-18 17:17:39 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 228 ms, Memory: 22900 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int height = Integer.parseInt(br.readLine());
+
+            while (!stack.isEmpty() && stack.peek() <= height) {
+                stack.pop();
+            }
+
+            answer += stack.size();
+
+            stack.push(height);
+        }
+
+        System.out.println(answer);
+    }
+}
