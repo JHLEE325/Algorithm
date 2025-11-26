4310e8b622804d553f53ee4de877f35dd6264572
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-11-26 16:42:49 +0700

    [Gold V] Title: 행복 유치원, Time: 452 ms, Memory: 62584 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int[] diff = new int[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+
+        Arrays.sort(diff);
+
+        int answer = 0;
+        for (int i = 0; i < N - K; i++) {
+            answer += diff[i];
+        }
+
+        System.out.println(answer);
+    }
+}
