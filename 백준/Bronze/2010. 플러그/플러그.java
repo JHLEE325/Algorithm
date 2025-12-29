901cfd08938917302f8666267942c3d6a0c90c43
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2025-12-29 20:16:22 +0700

    [Bronze III] Title: 플러그, Time: 208 ms, Memory: 39076 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int res = 0;
+        for(int i=0; i<n; i++){
+            res+=Integer.parseInt(br.readLine());
+        }
+        
+        System.out.println(res-n+1);
+    }
+}
