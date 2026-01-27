73a4f0a0c7ce80c059bd0973c720bd79c6a4d04e
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2026-01-27 22:47:47 +0900

    [Gold IV] Title: 음식 평론가, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int res = m - gcd(n, m);
+        
+        System.out.println(res);
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
