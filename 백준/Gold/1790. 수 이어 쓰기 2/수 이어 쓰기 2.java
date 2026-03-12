749e229401d16b3799fad821972e4f4934857e90
JHLEE325 <82587652+JHLEE325@users.noreply.github.com>
2026-03-12 16:55:59 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
@@ -9,37 +9,23 @@ public class Main {
         long N = Long.parseLong(st.nextToken());
         long K = Long.parseLong(st.nextToken());
 
-        if (calcLen(N) < K) {
+        long len = 1;
+        long count = 9;
+        long num = 0;
+
+        while (K > len * count) {
+            K -= (len * count);
+            len++;
+            count *= 10;
+        }
+
+        num = (long)Math.pow(10, len - 1) + (K - 1) / len;
+
+        if (num > N) {
             System.out.println("-1");
-            return;
+        } else {
+            String s = String.valueOf(num);
+            System.out.println(s.charAt((int)((K - 1) % len)));
         }
-
-        long low = 1, high = N;
-        long target = 0;
-        while (low <= high) {
-            long mid = (low + high) / 2;
-            if (calcLen(mid) >= K) {
-                target = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        String s = String.valueOf(target);
-        long beforeLen = calcLen(target - 1);
-        int index = (int) (K - beforeLen - 1);
-        
-        System.out.println(s.charAt(index));
-    }
-
-    static long calcLen(long n) {
-        long total = 0;
-        for (long start = 1, len = 1; start <= n; start *= 10, len++) {
-            long end = start * 10 - 1;
-            if (end > n) end = n;
-            total += (end - start + 1) * len;
-        }
-        return total;
     }
 }
